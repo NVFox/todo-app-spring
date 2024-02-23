adbb3661ffd060392a3ea89df1865c0e1842a373
ADT64 <davidroa8@hotmail.com>
2024-02-23 13:28:44 -0500

    feat(task): add task repository and getters/setters for task entity (lombok)

--- src/main/java/com/adtdev/todoapp/entities/Task.java
+++ src/main/java/com/adtdev/todoapp/entities/Task.java
@@ -2,11 +2,16 @@ package com.adtdev.todoapp.entities;
 
 import com.adtdev.todoapp.enums.TaskStatus;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
+@Getter
+@Setter
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
